6ad67f4dd9ed1e312549439bb4f69580d04d3ce0
Chengbo <577114589@qq.com>
2020-08-16 04:58:29 +0800

    开局界面

---
+++ Assets/Scripts/BeginGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginGame : MonoBehaviour
+{
+    public GameObject game;
+    public GameObject entrance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Beign()
+    {
+        game.SetActive(true);
+        entrance.SetActive(false);
+    }
+}
